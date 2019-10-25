373d8d7591cc0757ecdec63f667ca5249982ce14
Lynda Trad <55886684+LyndaTrad@users.noreply.github.com>
2019-10-25 18:40:07 +0200

    ajout d'une classe direction et futur ajout dune class coordonnées
    histoire de simplifier verification des collisions

---
+++ Direction.java
@@ -0,0 +1,20 @@
+package pacman;
+
+public class Direction 
+{
+	
+	//base sur snake -> à voir 
+	
+	// dedans je met un couple x y 
+	// up -> 0 , 1 
+	
+	//donc quand jaurai besoin de bouger je mettrai
+	// x += direction.x y += direction.y
+}
+
+// faire pareil pour coordonnées
+// contient un x et un y 
+
+// class qui ont besoin auront un Coordonnées c -> cet objet aura un x et y 
+// et on les recup avec des getters
+// pour faire plus propre
